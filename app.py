2ff79412b24d72a9e067fc4df09971b4807a6aef
Dfria <jdfria@gmail.com>
2021-11-12 15:32:04 -0800

    working account authentication

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import MySQLdb
 from flask import Flask, render_template, request, session, jsonify
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 import json
 
-
 app = Flask(__name__)
-db = MySQL(app)
+app.secret_key = "lesgo"
 
 app.config['MYSQL_USER'] = 'fjjcoreudogl1fxb'
 app.config['MYSQL_PASSWORD'] = 'zuiyjci4038yq91m'
@@ -17,12 +13,14 @@ app.config['MYSQL_HOST'] = 'xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaw
 app.config['MYSQL_DB'] = 'u9zs3sxzsl1bf3ly'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+db = MySQL(app)
 
 @app.route("/")
-def hello():
-    cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('''CREATE TABLE accounts (user_id INT(9), password VARCHAR(32), ''')
+def index():
+    # cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    # cursor.execute('''CREATE TABLE accounts (userid INTEGER, password VARCHAR(32))''')
+    # cursor.execute('''INSERT INTO accounts VALUES(30217890, "abc123")''')
+    # cursor.connection.commit()
     return render_template("form.html")
 
 
@@ -37,24 +35,19 @@ def login():
     if request.method == 'GET':
         return "Login via the login Form"
 
-    if request.method == 'POST' and 'account_id' in request.form and 'password' in request.form:
-        user_id = request.form['user_id']
+    if request.method == 'POST' and 'userid' in request.form and 'password' in request.form:
+        userid = request.form['userid']
         password = request.form['password']
         cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('''CREATE TABLE accounts (user_id INT(9), password VARCHAR(32), ''')
-        cursor.execute('SELECT * FROM accounts WHERE user_id = %s AND password = %s', (user_id, password))
+        # cursor.execute('''CREATE TABLE accounts (userid INTEGER, password VARCHAR(32))''')
+        cursor.execute('SELECT * FROM accounts WHERE userid = %s AND password = %s', (userid, password, ))
         account = cursor.fetchone()
         if account:
             session['loggedin'] = True
-            session['user_id'] = account['user_id']
+            session['userid'] = account['userid']
             session['password'] = account['password']
-            return 'Logged in successfully !'
+            msg = 'logged in successfully.'
+            return render_template('scheduler.html', msg=msg)
         else:
             msg = 'Incorrect username / password!'
-            return render_template('formfailed.html')
-        db.connection.commit()
-        cursor.close()
-    return render_template('scheduler.html')
-    db.connection.commit()
-    cursor.close()
-
+    return render_template('form.html', msg=msg)
